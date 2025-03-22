5199e9738ecd99d3647c07bac7a5e00feca6fff9
dvdmarveira <ddmdoliveira@gmail.com>
2025-03-22 15:28:28 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./pages/**/*.html", // Para pegar todos os arquivos HTML na pasta "pages"
+    "./assets/styles/**/*.css", // Para pegar os arquivos CSS na pasta "styles"
+    "./scripts/**/*.css", // Para pegar todos os arquivos JavaScript na pasta "scripts"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
